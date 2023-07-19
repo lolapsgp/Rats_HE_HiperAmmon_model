ad742dcef2f8598adc582aca6ab2594bb6d2dc3a
lolapsgp <lolaginerperez@hotmail.com>
2023-07-19 09:16:23 +0200

    corrections assigning taxonomy

--- R/Pricrust2_ratshiperammon.R
+++ R/Pricrust2_ratshiperammon.R
@@ -20,7 +20,7 @@ tmp[,2]<- paste0("ASV", 1:nrow(asvmat))
 colnames(tmp)<- c("Sequence", "ASV")
 rownames(asvmat) <- paste0("ASV", 1:nrow(asvmat))
 biom.tmp<- make_biom(asvmat, matrix_element_type = "int")
-write_biom(biom.tmp, biom_file = "/fast/AG_Forslund/Lola/Datos_CIPF_2023/Rats_HE_HiperAmmon_model/input/asv_ratshiperammon.biom")
+write_biom(biom.tmp, biom_file = "/fast/AG_Forslund/Lola/Datos_CIPF_2023/Rats_HE_HiperAmmon_model/input_ratshiperammon/asv_ratshiperammon.biom")
 
 
 ##Sequences with corrected names
@@ -28,4 +28,4 @@ library(DECIPHER)
 
 dna <- readDNAStringSet("/fast/AG_Forslund/Lola/Datos_CIPF_2023/Rats_HE_HiperAmmon_model/outputs/ASV.fasta")
 names(dna)<- paste0("ASV", 1:length(dna)) 
-writeXStringSet(dna, filepath = "/fast/AG_Forslund/Lola/Datos_CIPF_2023/Rats_HE_HiperAmmon_model/input/dna_ratshiperammon.fasta") 
+writeXStringSet(dna, filepath = "/fast/AG_Forslund/Lola/Datos_CIPF_2023/Rats_HE_HiperAmmon_model/input_ratshiperammon/dna_ratshiperammon.fasta") 
